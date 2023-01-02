0f28143598c61c95dcfd4cc44dab95b75f99ab34
David Ramos <davidramos.13@outlook.com>
2023-01-02 16:24:12 -0300

    vite template & tailwind twin macro setup

---
+++ 25/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import ViteReact from './components/ViteReact';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <ViteReact />
+    </div>
+  );
+};
+
+export default App;
---
+++ 25/frontend/src/components/ViteReact.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import tw from 'twin.macro';
+import reactLogo from '../assets/react.svg';
+
+const DivContainer = tw.div`fixed right-0 bottom-0`;
+const ALogo = tw.a`inline-block`;
+const ImgLogo = tw.img`h-32 p-4 will-change-auto hover:drop-shadow-blue`;
+
+const ReactLogo = tw(ImgLogo)`animate-spin-slow`;
+
+const ViteReact: React.FC = () => {
+
+  return (
+    <DivContainer>
+      <ALogo href="https://vitejs.dev" target="_blank">
+        <ImgLogo src="/vite.svg" alt="Vite logo" />
+      </ALogo>
+      <ALogo href="https://reactjs.org" target="_blank">
+        <ReactLogo src={reactLogo} alt="React logo" />
+      </ALogo>
+    </DivContainer>
+  );
+};
+
+export default ViteReact;
