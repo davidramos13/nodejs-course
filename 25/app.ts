8019923f6679d608639edf050612842dd57b079d
David Ramos <davidramos.13@outlook.com>
2023-01-02 11:24:50 -0300

    25 init

---
+++ 25/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import feedRoutes from './routes/feed';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.listen(8080);
+console.log('SERVER READY');
